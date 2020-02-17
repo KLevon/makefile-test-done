98bfc11a4c4178b9e09cae0cd176000c58eec55e
KLevon <42465755+KLevon@users.noreply.github.com>
2020-02-17 09:45:18 +0100

    Update and rename config.mk to rules.mk

--- rules.mk
+++ rules.mk
@@ -3,8 +3,8 @@ $(BUILD_DIR)/%.exe: $(OBJ_FILES) $(LIB_FULL)
 	gcc -o $@ $(OBJ_FILES) $(LIB_PATH_LINK) $(LIB_LINK)
 
 $(BUILD_DIR)/%.a: $(OBJ_FILES)
-	ar -rvs $@ $(OBJ_FILES)
+	ar -rcs $@ $(OBJ_FILES)
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.c $(HEADERS)
-	gcc -c $< -o $@ $(HEAD_LINK) 
+	gcc -c $< -o $@ $(HEAD_LINK)
 
